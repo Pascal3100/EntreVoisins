44f147dfed3eac964e431e06bccc186679bb6225
palopez <lopez.pascal.pro@gmail.com>
2021-03-18 16:17:24 +0100

    create and use of variables in layout

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_details/NeighbourDetailsActivity.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_details/NeighbourDetailsActivity.java
@@ -3,9 +3,11 @@ package com.openclassrooms.entrevoisins.ui.neighbour_details;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.openclassrooms.entrevoisins.R;
 
 public class NeighbourDetailsActivity extends AppCompatActivity {
@@ -18,11 +20,26 @@ public class NeighbourDetailsActivity extends AppCompatActivity {
     TextView cardCoordinatesSocial = findViewById(R.id.card_coordinates_social_neighbour_details_activity);
     TextView cardAboutTitle = findViewById(R.id.card_about_title_neighbour_details_activity);
     TextView cardAboutDescription = findViewById(R.id.card_about_description_neighbour_details_activity);
+    FloatingActionButton addFavorite = findViewById(R.id.add_favorite_fab_neighbour_details_activity);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neighbour_details);
 
+        /*Filling the different widgets*/
+
+
+
+
+
+
+        /*If FAB is pressed*/
+        addFavorite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*TODO implémenter ici l'ajout du voisin à la liste des favoris*/
+            }
+        });
     }
 }
