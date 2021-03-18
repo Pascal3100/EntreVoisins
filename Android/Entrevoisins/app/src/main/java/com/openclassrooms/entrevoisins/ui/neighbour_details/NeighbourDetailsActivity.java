3d67aea496abfce74ea1fd4e6641021fe30cf7aa
palopez <lopez.pascal.pro@gmail.com>
2021-03-18 10:34:04 +0100

    referencing all the widgets in java file

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_details/NeighbourDetailsActivity.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_details/NeighbourDetailsActivity.java
@@ -3,12 +3,26 @@ package com.openclassrooms.entrevoisins.ui.neighbour_details;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.openclassrooms.entrevoisins.R;
 
 public class NeighbourDetailsActivity extends AppCompatActivity {
+
+    ImageView appbarBackgroundImage = findViewById(R.id.appbar_background_image_neighbour_details_activity);
+    TextView bigTitleNeighbour = findViewById(R.id.big_title_neighbour_details_activity);
+    TextView cardCoordinatesTitle = findViewById(R.id.card_coordinates_title_neighbour_details_activity);
+    TextView cardCoordinatesAdress = findViewById(R.id.card_coordinates_adress_neighbour_details_activity);
+    TextView cardCoordinatesPhone = findViewById(R.id.card_coordinates_phone_neighbour_details_activity);
+    TextView cardCoordinatesSocial = findViewById(R.id.card_coordinates_social_neighbour_details_activity);
+    TextView cardAboutTitle = findViewById(R.id.card_about_title_neighbour_details_activity);
+    TextView cardAboutDescription = findViewById(R.id.card_about_description_neighbour_details_activity);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neighbour_details);
+
     }
 }
