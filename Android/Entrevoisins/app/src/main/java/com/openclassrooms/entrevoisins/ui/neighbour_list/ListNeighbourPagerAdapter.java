2f54361ae88a7b1cb48c0796a07e5a9a75af097e
palopez <lopez.pascal.pro@gmail.com>
2021-03-19 14:59:49 +0100

    manage deprecated FragmentPagerManager

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/ListNeighbourPagerAdapter.java
@@ -1,13 +1,20 @@
 package com.openclassrooms.entrevoisins.ui.neighbour_list;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 
 public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
 
+    /*Managing deprecated FragmentPagerAdapter to the new one*/
+
     public ListNeighbourPagerAdapter(FragmentManager fm) {
-        super(fm);
+
+        super(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+
+        /*Deprecated FragmentPagerAdapter constructor*/
+        /* super(fm); */
     }
 
     /**
@@ -15,6 +22,7 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
      * @param position
      * @return
      */
+    @NonNull
     @Override
     public Fragment getItem(int position) {
         return NeighbourFragment.newInstance();
@@ -28,4 +36,30 @@ public class ListNeighbourPagerAdapter extends FragmentPagerAdapter {
     public int getCount() {
         return 1;
     }
+
+    /*Deprecated FragmentPagerAdapter methods*/
+
+    /**
+       * getItem is called to instantiate the fragment for the given page.
+       * @param position
+       * @return
+       */
+    /*
+    @Override
+    public Fragment getItem(int position) {
+        return NeighbourFragment.newInstance();
+    }
+    */
+
+    /**
+     * get the number of pages
+     * @return
+     */
+    /*
+    @Override
+    public int getCount() {
+        return 1;
+    }
+    */
+
 }
