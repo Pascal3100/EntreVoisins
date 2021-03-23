25c6b9db1aa1e401146c832d41fa6de860261e6d
palopez <lopez.pascal.pro@gmail.com>
2021-03-23 11:28:12 +0100

    Refactor of the class ViewHolder in a separate file and rename into NeighbourRecyclerViewViewHolder

--- Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/MyNeighbourRecyclerViewAdapter.java
@@ -3,9 +3,6 @@ package com.openclassrooms.entrevoisins.ui.neighbour_list;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -19,10 +16,7 @@ import org.greenrobot.eventbus.EventBus;
 
 import java.util.List;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeighbourRecyclerViewAdapter.ViewHolder> {
+public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<NeighbourRecyclerViewViewHolder> {
 
     private final List<Neighbour> mNeighbours;
 
@@ -31,14 +25,14 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
     }
 
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public NeighbourRecyclerViewViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.fragment_neighbour, parent, false);
-        return new ViewHolder(view);
+        return new NeighbourRecyclerViewViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(final ViewHolder holder, int position) {
+    public void onBindViewHolder(final NeighbourRecyclerViewViewHolder holder, int position) {
         Neighbour neighbour = mNeighbours.get(position);
         holder.mNeighbourName.setText(neighbour.getName());
         Glide.with(holder.mNeighbourAvatar.getContext())
@@ -57,19 +51,5 @@ public class MyNeighbourRecyclerViewAdapter extends RecyclerView.Adapter<MyNeigh
     @Override
     public int getItemCount() {
         return mNeighbours.size();
-    }
-
-    public class ViewHolder extends RecyclerView.ViewHolder {
-        @BindView(R.id.item_list_avatar)
-        public ImageView mNeighbourAvatar;
-        @BindView(R.id.item_list_name)
-        public TextView mNeighbourName;
-        @BindView(R.id.item_list_delete_button)
-        public ImageButton mDeleteButton;
-
-        public ViewHolder(View view) {
-            super(view);
-            ButterKnife.bind(this, view);
-        }
     }
 }
---
+++ Android/Entrevoisins/app/src/main/java/com/openclassrooms/entrevoisins/ui/neighbour_list/NeighbourRecyclerViewViewHolder.java
@@ -0,0 +1,27 @@
+package com.openclassrooms.entrevoisins.ui.neighbour_list;
+
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.openclassrooms.entrevoisins.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class NeighbourRecyclerViewViewHolder extends RecyclerView.ViewHolder {
+    @BindView(R.id.item_list_avatar)
+    public ImageView mNeighbourAvatar;
+    @BindView(R.id.item_list_name)
+    public TextView mNeighbourName;
+    @BindView(R.id.item_list_delete_button)
+    public ImageButton mDeleteButton;
+
+    public NeighbourRecyclerViewViewHolder(View view) {
+        super(view);
+        ButterKnife.bind(this, view);
+    }
+}
